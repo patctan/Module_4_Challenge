ed9f46b90d6b9bb09a755f69620d89f5ed004819
Bitaw <stickynotes5@yahoo.com>
2022-07-28 14:55:20 -0500

    Added comments in html to plan out how to write the code

---
+++ script.js
@@ -0,0 +1,6 @@
+//Set questions as arrays, then do if-else for continuation?
+
+var questions = document.getElementById("question");
+
+questions = ["persona", "emblem", "xenoblade", "fluff"];
+//there's a display function?
